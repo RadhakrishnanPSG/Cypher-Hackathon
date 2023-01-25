d16f6187e4731d2cdf6cd6d8f18b2ecbdf8e032c
Radhakrishnan M <76259621+RadhakrishnanPSG@users.noreply.github.com>
2023-01-25 15:02:21 +0530

    Add files via upload

--- Seed.js
+++ Seed.js
@@ -10,7 +10,10 @@ export default function Seed ({navigation}) {
     var keys = key.split(' ')
     var status = 0
     //const hdWallet = wallet.fromMnemonic(key)
-    
+    //wallet = hdWallet.derive(`m/44/60/'/0'/0`)
+    //mywallet.derive(1).getAddress().toString('hex')
+    //mywallet.derive(2).getAddress().toString('hex')
+    //mywallet.derive(3).getAddress().toString('hex')
   
     const hide = () => {
       for(var i =0;i<12;i++)
